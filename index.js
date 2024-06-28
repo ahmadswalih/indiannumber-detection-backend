4c2d465f470d9639bc53cab586ae134b4eaad145
ahmadswalih <ahmadswalih777@gmail.com>
2024-06-28 22:26:36 +0530

    iniital commit

---
+++ index.js
@@ -0,0 +1,161 @@
+const express = require("express");
+const multer = require("multer");
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Make sure this folder exists
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ storage: storage });
+
+// Dummy user data associated with verified numbers
+const userData = {
+  DL7CD5017: {
+    name: "Abhijith",
+    email: "abhijith@gmail.com",
+    phone: "7736012345",
+  },
+  // Add more user data as needed
+};
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No video file uploaded.");
+  }
+
+  const videoPath = req.file.path;
+  const duration = parseInt(req.body.duration, 10) || 10; // Default to 10 if duration is not provided
+
+  ffmpeg.ffprobe(videoPath, (err, metadata) => {
+    if (err) {
+      console.error("Error getting video metadata:", err);
+      return res.status(500).send("Error getting video metadata.");
+    }
+
+    const videoDuration = metadata.format.duration;
+    const frameCount = Math.min(duration, videoDuration); // Ensure we don't request more frames than the video duration
+    const width = metadata.streams[0].width;
+    const height = metadata.streams[0].height;
+    ffmpeg(videoPath)
+      .on("end", function () {
+        console.log("Frames extraction completed.");
+        // Process the extracted frames
+        processFrames(res);
+      })
+      .on("error", function (err) {
+        console.error("Error extracting frames:", err);
+        res.status(500).send("Error extracting frames.");
+      })
+      .screenshots({
+        count: frameCount,
+        folder: "uploads/",
+        filename: "frame-at-%s-seconds.png",
+        size: `${width}x${height}`,
+      });
+  });
+});
+
+function processFrames(res) {
+  const frames = fs
+    .readdirSync("uploads/")
+    .filter((file) => file.startsWith("frame-"));
+  const promises = [];
+  const verifiedNumbers = Object.keys(userData);
+
+  frames.forEach((frame) => {
+    const framePath = `uploads/${frame}`;
+    console.log(`Processing frame: ${framePath}`);
+
+    const formData = new FormData();
+    formData.append("image", fs.createReadStream(framePath));
+    formData.append("country", "us");
+
+    const headers = {
+      ...formData.getHeaders(),
+      Accept: "application/json",
+      "x-rapidapi-ua": "RapidAPI-Playground",
+      "x-rapidapi-key": "7f624d6b87msh92e2c1a4911e73dp1c2a08jsn03988521d285",
+      "x-rapidapi-host": "openalpr.p.rapidapi.com",
+    };
+
+    // Send each frame to OpenALPR API for processing
+    promises.push(
+      axios.post(
+        "https://openalpr.p.rapidapi.com/recognize?country=in",
+        formData,
+        { headers }
+      )
+    );
+  });
+
+  // Wait for all API requests to finish
+  Promise.all(promises)
+    .then((responses) => {
+      const results = responses
+        .map((response, index) => {
+          const plateResult = response.data.results[0]; // Assuming the first result is the most relevant
+          if (!plateResult) return null; // Skip if no plate result
+
+          const plate = plateResult.plate;
+          // Skip if plate number length is less than 8
+          if (plate.length < 6) return null;
+          let authenticationStatus = "New Visitor";
+          let userAssociatedData = null;
+
+          if (verifiedNumbers.includes(plate)) {
+            authenticationStatus = "Verified";
+            userAssociatedData = userData[plate];
+          }
+
+          return {
+            //frame: frames[index],
+            plate: plate,
+            confidence: plateResult.confidence,
+            authentication: authenticationStatus,
+            user: userAssociatedData,
+          };
+        })
+        .filter((result) => result !== null); // Filter out results with no plate
+
+      res.json(results);
+      // Clean up uploaded files after processing
+      cleanUpFiles();
+    })
+    .catch((error) => {
+      console.error("Error processing frames:", error);
+      res.status(500).send("Error processing frames.");
+      // Clean up uploaded files after processing
+      cleanUpFiles();
+    });
+}
+
+function cleanUpFiles() {
+  // Delete uploaded files after processing
+  const files = fs.readdirSync("uploads/");
+  files.forEach((file) => {
+    fs.unlinkSync(`uploads/${file}`);
+  });
+}
+
+app.get("/health", (req, res) => {
+  res.send("I am healthy");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
